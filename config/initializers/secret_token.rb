d663955dadb8b7778db5106fba36a6482c9d9f0b
brett-ferencz <brett.ferencz@turner.com>
2014-03-21 18:24:40 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScotchNew::Application.config.secret_key_base = 'f84bdfa33a0cbf1ce32b8c5d5f85ee191ef19d619fc6857ee5321860eaf8766a383ea6c62ef2dd705838da01f26ad98b5cf827c5f30636c11b8ca17e49022dea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScotchNew::Application.config.session_store :cookie_store, key: '_scotch_new_session'
